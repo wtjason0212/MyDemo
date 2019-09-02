219066dad66a2298b37b64c3f52a11e1c52b261e
jay.hsu <gn0039625@gmail.com>
2019-09-03 01:23:53 +0800

    新增kafkamq及eventbus介面

---
+++ EvenBus/Abstractions/IIntegrationEventHandler.cs
@@ -0,0 +1,19 @@
+﻿using EvenBus.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvenBus.Abstractions
+{
+    public interface IIntegrationEventHandler<in TIntegrtionEvent> : IIntegrationEventHandler 
+        where TIntegrtionEvent : IntegrationEvent
+    {
+        Task Handle(TIntegrtionEvent @event);
+    }
+
+    public interface IIntegrationEventHandler
+    {
+
+    }
+}
---
+++ EventBusKafkaMQ/EventBusKafka.cs
@@ -0,0 +1,60 @@
+﻿using Confluent.Kafka;
+using EvenBus.Abstractions;
+using EvenBus.Events;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventBusKafkaMQ
+{
+    public class EventBusKafka : IEventBus, IDisposable
+    {
+        const string TOPIC_NAME = "my-testtopic";
+        private readonly ILogger<EventBusKafka> _logger;
+
+        public EventBusKafka(ILogger<EventBusKafka> logger)
+        {
+            _logger = logger;
+        }
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async void Publish(IntegrationEvent @event)
+        {
+            //判斷連線
+            //todo
+
+            //publish
+            var config = new ProducerConfig { BootstrapServers = "localhost:9092" };
+
+            // If serializers are not specified, default serializers from
+            // `Confluent.Kafka.Serializers` will be automatically used where
+            // available. Note: by default strings are encoded as UTF8.
+            using (var p = new ProducerBuilder<Null, string>(config).Build())
+            {
+                try
+                {
+                    var dr = await p.ProduceAsync(TOPIC_NAME, new Message<Null, string> { Value = @event.Id });
+                    Console.WriteLine($"Delivered '{dr.Value}' to '{dr.TopicPartitionOffset}'");
+                }
+                catch (ProduceException<Null, string> e)
+                {
+                    Console.WriteLine($"Delivery failed: {e.Error.Reason}");
+                }
+            }
+
+
+            throw new NotImplementedException();
+        }
+
+        public void Subscribe<T, TH>()
+            where T : IntegrationEvent
+            where TH : IIntegrationEventHandler<T>
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
