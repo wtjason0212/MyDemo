7edae549413d5d2b3b4e3b8ed8cc9964c9788bc8
wtjason0212 <candygo159@gmail.com>
2019-09-03 14:04:19 +0800

    新增testbackgroundtask

---
+++ WebApplication1/Tasks/TestBackgroundTask.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Tasks
+{
+    public class TestBackgroundTask : BackgroundService
+    {
+        private readonly ILogger<TestBackgroundTask> _logger;
+        private Task _executingTask;
+        private readonly CancellationTokenSource _stoppingCts = new CancellationTokenSource();
+        public TestBackgroundTask (ILogger<TestBackgroundTask> logger)
+        {
+            _logger = logger;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            _logger.LogDebug($"Test Background Task Start");
+            stoppingToken.Register(() =>
+            _logger.LogDebug($"Test Background Task Stop")
+            ) ;
+
+            while(!stoppingToken.IsCancellationRequested)
+            {
+                _logger.LogDebug("Test Task Doing Background Work");
+
+                await Task.Delay(100,stoppingToken);
+            }
+
+            throw new NotImplementedException();
+        }
+
+        public virtual Task StartAsync(CancellationToken stoppingToken)
+        {
+            _executingTask = ExecuteAsync(_stoppingCts.Token);
+
+            if(_executingTask.IsCompleted)
+            {
+                return _executingTask;
+            }
+
+            return Task.CompletedTask;
+        }
+
+        public virtual async Task StopAsync(CancellationToken stoppingToken)
+        {
+            if(_executingTask == null)
+            {
+                return ;
+            }
+
+            try
+            {
+                _stoppingCts.Cancel();
+            }
+            catch(Exception ex)
+            {
+                _logger.LogDebug($"Stop Error:{ex.Message} ");
+            }
+            finally
+            {
+                await Task.WhenAny(_executingTask, Task.Delay(100));
+            }
+
+        }
+
+        public virtual void Dispose()
+        {
+            _stoppingCts.Cancel();
+        }
+    }
+}
