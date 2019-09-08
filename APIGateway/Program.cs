26a56c42ab6809a0dfecbebbd21ac5087f18e96e
tcjason <twjason@dafacloud.com>
2019-09-08 22:12:51 +0800

    Ocelot

---
+++ APIGateway/Program.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+
+namespace APIGateway
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+        public static IWebHost BuildWebHost(string[] args)
+        {
+            return WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .UseUrls($"http://localhost:9000")
+                .ConfigureAppConfiguration((hostingContext, builder) => { builder.AddJsonFile("configuration.json", false, true); })
+                .Build();
+        }
+    }
+
+
+    //public class Program
+    //{
+    //    public static void Main(string[] args)
+    //    {
+    //        //CreateWebHostBuilder(args).Run();
+    //        IWebHostBuilder builder = new WebHostBuilder();
+    //        builder.ConfigureServices(s =>
+    //        {
+    //            s.AddSingleton(builder);
+    //        });
+    //        builder.UseKestrel()
+    //               .UseContentRoot(Directory.GetCurrentDirectory())
+    //               .UseStartup<Startup>()
+    //               .UseUrls("http://localhost:9000");
+
+    //        var host = builder.Build();
+    //        host.Run();
+    //    }
+
+    //    public static IWebHost CreateWebHostBuilder(string[] args)
+    //    {
+    //      return  WebHost.CreateDefaultBuilder(args)
+    //            .UseStartup<Startup>()
+    //            .UseUrls($"http://localhost:55954")
+    //            .ConfigureAppConfiguration((hostingContext,builder)=>
+    //            {
+    //                builder.AddJsonFile("configuration.json", false, true);
+    //            })
+    //            .Build();
+    //    }
+
+    //}
+}
---
+++ ClientService/Controllers/ClientController.cs
@@ -0,0 +1,22 @@
+﻿namespace CustomersAPIServices.Controllers
+{
+
+    using System.Collections.Generic;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("api/[controller]")]
+    public class ClientController : Controller
+    {
+        [HttpGet] public IEnumerable<string> Get()
+        {
+            return new string[] { "ClinetService-value1", "ClinetService-value2" };
+        }
+
+
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return $"Catcher Wong - {id}";
+        }
+    }
+}
